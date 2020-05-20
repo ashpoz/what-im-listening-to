e6a8c3fff6097f29f23e32e44dbf048e250b1cd9
hellomaryjo <majorx3music@gmail.com>
2020-05-20 18:01:46 -0500

    figuring out how spotify api works. Need to move to functions folder so info is secure

---
+++ functions/spotify-access.js
@@ -0,0 +1,16 @@
+exports.handler = (event, _context, callback) => {
+    // console.log({ event });
+
+    // callback(null, {
+    //     statusCode: 200,
+    //     body: JSON.stringify({ boop: true })
+    // });
+
+    // TODO: 
+
+    // 1. authorize and receive access token
+
+    // 2. use token to receive data
+
+    // 3. output data on this page so it can be accessed
+};
