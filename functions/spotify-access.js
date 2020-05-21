7289882d4ee92f77035cb6075e9b9a0b273854ce
hellomaryjo <majorx3music@gmail.com>
2020-05-20 23:28:48 -0500

    got client cred workflow to work, next refactor for auth code workflow

--- functions/spotify-access.js
+++ functions/spotify-access.js
@@ -1,16 +1,51 @@
+const axios = require("axios");
+
 exports.handler = (event, _context, callback) => {
-    // console.log({ event });
-
-    // callback(null, {
-    //     statusCode: 200,
-    //     body: JSON.stringify({ boop: true })
-    // });
-
     // TODO: 
 
-    // 1. authorize and receive access token
+    // ** IMPORTANT ** looks like a need the authorization code flow in order
+    // to view my own listening patterns. See if you can do this. 
+    
+    // 1. get most recent track
+    // 2. get top 10 current + all time artists
+    // 3. get top 10 current + all time tracks
 
-    // 2. use token to receive data
+    const getSpotifyData = (token) => {
+        axios({
+            method: "GET",
+            url: "https://api.spotify.com/v1/me/player",
+            headers: {
+                "Authorization": "Bearer " + token,
+            }
+        })
+        .then(res => send(res.data)) // parse JSON from request
+        .catch(err => send(err));
+    }
 
-    // 3. output data on this page so it can be accessed
+    const getAccessToken = () => {
+        axios({
+            method: "POST",
+            url: "https://accounts.spotify.com/api/token",
+            headers: {
+                "Authorization": "Basic " + (new Buffer(process.env.GATSBY_CLIENT_ID + ":" + process.env.GATSBY_CLIENT_SECRET).toString('base64')),
+                "Content-Type": "application/x-www-form-urlencoded", 
+            },
+            data: "grant_type=client_credentials",
+        })
+        .then(res => {
+            getSpotifyData(res.data.access_token)
+        }) 
+        .catch(err => send(err));
+    }
+
+    const send = body => {
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(body)
+        });
+    }
+
+    // fn calls
+    getAccessToken();
+
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,34 +4,84 @@ import Layout from '../components/layout';
 import Header from '../components/header';
 import Img from "gatsby-image";
 
+const axios = require("axios");
+
+
 const Home = ({data}) => {
+
+
+  const getSpotifyData = (token) => {
+    axios({
+        method: "GET",
+        url: "https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl",
+        headers: {
+            "Authorization": "Bearer " + token,
+            "Content-Type": 'application/x-www-form-urlencoded',
+        }
+    })
+    .then(res => console.log(res)) // parse JSON from request
+    .catch(err => err);
+}
+
+const getAccessToken = () => {
+    axios({
+        method: "POST",
+        url: "https://accounts.spotify.com/api/token",
+        data: "grant_type=client_credentials",
+        headers: {
+            "Authorization": "Basic " + (new Buffer(process.env.GATSBY_CLIENT_ID + ":" + process.env.GATSBY_CLIENT_SECRET).toString('base64')),
+            "Content-Type": "application/x-www-form-urlencoded", 
+        }
+    })
+    .then(res => {
+      console.log(res)
+      getSpotifyData(res.data.access_token)
+
+    }) // parse JSON from request
+    // .then(result => {
+    //   console.log(result.access_token);
+    //   getSpotifyData(result.access_token)
+    // }) 
+
+    .catch(err => err);
+}
+
+// const send = body => {
+//     callback(null, {
+//         statusCode: 200,
+//         body: JSON.stringify(body)
+//     });
+// }
+
+// fn calls
+getAccessToken();
 
     // TODO: move this to functions folder 
     
     // authorize and receive access token
-    fetch(`https://accounts.spotify.com/api/token`, {
-      method: 'POST',
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(process.env.GATSBY_CLIENT_ID + ':' + process.env.GATSBY_CLIENT_SECRET).toString('base64')),
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `grant_type=client_credentials`
-    })
-      .then(response => response.json()) // parse JSON from request
-      .then(resultData => {
-        let accessToken = resultData.access_token;
-        console.log(accessToken);
+    // fetch(`https://accounts.spotify.com/api/token`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Authorization': 'Basic ' + (new Buffer(process.env.GATSBY_CLIENT_ID + ':' + process.env.GATSBY_CLIENT_SECRET).toString('base64')),
+    //     'Content-Type': 'application/x-www-form-urlencoded',
+    //   },
+    //   body: `grant_type=client_credentials`
+    // })
+    //   .then(response => response.json()) // parse JSON from request
+    //   .then(resultData => {
+    //     let accessToken = resultData.access_token;
+    //     console.log(accessToken);
 
-        fetch(`https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl`, {
-          method: 'GET',
-          headers: {
-            'Authorization': 'Bearer ' + accessToken,
-            'Content-Type': 'application/x-www-form-urlencoded',
-          }
-      }) 
-      .then(body => body.json()) // parse JSON from request
-      .then(data => console.log(data))
-    })
+    //     fetch(`https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl`, {
+    //       method: 'GET',
+    //       headers: {
+    //         'Authorization': 'Bearer ' + accessToken,
+    //         'Content-Type': 'application/x-www-form-urlencoded',
+    //       }
+    //   }) 
+    //   .then(body => body.json()) // parse JSON from request
+    //   .then(data => console.log(data))
+    // })
 
     
 
